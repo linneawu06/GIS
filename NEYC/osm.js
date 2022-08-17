7396af4c8991ba19a77150c0bd43989aee2f765b
name <100189793+linneawu05@users.noreply.github.com>
2022-08-17 23:51:42 +0800

    test

---
+++ NEYC/osm.js
@@ -0,0 +1,16 @@
+var map = L.map('map').setView([23.9, 121], 8);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/">OSM</a>',
+    maxZoom:8,
+}).addTo(map);
+
+// // OrthoPhotomap
+// var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+// 	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+// });
+
+// // drakmap
+// var Stadia_AlidadeSmoothDark = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
+// 	maxZoom: 8,
+//  attribution: '&copy; <a href="https://www.openstreetmap.org/">OSM</a>',
+// });
